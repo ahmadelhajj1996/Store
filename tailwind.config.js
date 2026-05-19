9299403de85ca70878044ef4cc7dd716621eec9b
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-05-19 05:32:38 -0700

    Change 'sm' screen size from 640px to 440px

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      sm: '640px',
+      sm: '440px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
