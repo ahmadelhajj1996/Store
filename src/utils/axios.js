1c7d1fcf8e22d65c7c2ef3e9f984339a751ba23d
Ahmad Elhaj <139491035+ahmadelhajj1996@users.noreply.github.com>
2026-05-30 15:02:10 -0700

    Update API base URL to production server

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import { store } from "../store/store";
 
 const api = axios.create({
-  // baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
-  baseURL: "http://127.0.0.1:8000/api/",
+    baseURL: "https://phplaravel-1626350-6427540.cloudwaysapps.com/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
 
   headers: {
     "Content-Type": "multipart/form-data",
